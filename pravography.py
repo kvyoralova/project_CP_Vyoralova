fa4b5f0b5b7b4588401976286b340f0f4952353b
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-05-26 22:58:10 +0200

    Update pravography.py

--- pravography.py
+++ pravography.py
@@ -5,10 +5,9 @@ st.write('Грайте, пишіть і вчіться! Gioca, scrivi e impara!'
 number = st.slider("Scegli un numero da 1 a 10", min_value=1, max_value=10)
 random = st.button("Oppure clicca qui per avere 5 immagini casuali")
 number = str(number)
-st.write(type(number))
 
-#chosen_dataset = {'1':['nomi immagini con 1.'],
-#                  '2':[],
+chosen_dataset = {'1':['1. Arancia.jpeg', '1. Bacio.jpeg', '1. Cetriolo.jpeg', '1. Ciuccio.jpeg', '1. Dieci.jpeg']}
+#                  '2':['prova', 'cane', 'gatto', 'akj', 'slkdf'],
 #                  '3':[],
 #                  '4':[],
 #                  '5':[],
@@ -17,4 +16,20 @@ st.write(type(number))
 #                  '8':[],
 #                  '9':[],
 #                  '10':[]}
-#random_dataset = unire tutte le liste dei values di chosen_dataset e scegliere 5 elementi casuali
+
+import numpy
+all_images = list()
+for myList in chosen_dataset.values():
+    for image in myList:
+        all_images.append(image)
+random_dataset = numpy.random.choice(all_images, 5, False)
+
+if number:
+  image_dataset = chosen_dataset(number)
+elif random:
+  image_dataset = random_dataset
+else:
+  pass
+
+for image in image_dataset:
+  st.image(image)
