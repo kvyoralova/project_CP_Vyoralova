deaf4cabcfdeccd5a0e671f1ac6abaa073de47ce
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-05-26 22:34:57 +0200

    Update pravography.py

--- pravography.py
+++ pravography.py
@@ -1,5 +1,18 @@
+import streamlit as st
 st.title('Pravografia: навчись добре писати італійською! Impara a scrivere bene in italiano!')
 st.write('Грайте, пишіть і вчіться! Gioca, scrivi e impara!')
 
 number = st.slider("Scegli un numero da 1 a 10", min_value=1, max_value=10)
 random = st.button("Oppure clicca qui per avere 5 immagini casuali")
+
+#chosen_dataset = {'1':['nomi immagini con 1.'],
+#                  '2':[],
+#                  '3':[],
+#                  '4':[],
+#                  '5':[],
+#                  '6':[],
+#                  '7':[],
+#                  '8':[],
+#                  '9':[],
+#                  '10':[]}
+#random_dataset = unire tutte le liste dei values di chosen_dataset e scegliere 5 elementi casuali
