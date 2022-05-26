fbd7053ceca7242bef503ead9d28f608f2de6e3e
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-05-26 22:21:02 +0200

    Update pravography.py

--- pravography.py
+++ pravography.py
@@ -1,14 +1,5 @@
-#Dataset di una 50ina di immagini con parola in ucraino.
-#L'utente sceglie un numero da 1 a 10, a ogni numero è abbinata una difficoltà ortografica sulle 5 vocali+gruppo consonantico
-#+ opzione randomizza (pesca 5 immagini random)
-#Traduzione della parola in italiano, che viene mostrata a video + pronuncia corretta in italiano.
-#Spariscono tutte le immagini e rimangono solo le parole in ucraino con un box dove scriverle in italiano.
-#Il programma fa lo spell check.
-#Poi verifica quante ne ha scritte di giuste:
-#  se più del 60% gli dice bravo in ucraino e in italiano e mostra uno smile felice;
-#  se meno del 60% gli dice riprova in italiano e in ucraino e mostra gli errori
-#Poi volevo mettere un pulsante di reset per giocare di nuovo
-#L’obiettivo è quello di associare il suono all’ortografia italiana e aiutare a migliorare nella scrittura
-#
-#Come target ho scelto bambini ucraini di livello scolare elementare che sanno leggere
+st.title('Pravografia: навчись добре писати італійською! Impara a scrivere bene in italiano!')
+st.write('Грайте, пишіть і вчіться! Gioca, scrivi e impara!')
 
+number = st.slider("Scegli un numero da 1 a 10", min_value=1, max_value=10)
+random = st.button("Oppure clicca qui per avere 5 immagini casuali")
