22ee48e1b20516e95c9d2b704dc82347b53f62f2
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-05-29 18:17:24 +0200

    Update pravography.py

--- pravography.py
+++ pravography.py
@@ -80,32 +80,36 @@ for image_dict in image_dataset:
   for image, text in image_dict.items():
     st.image(image)
     st.write('Як ви говорите те, що бачите українською? Come si dice in ucraino la cosa che vedi?')
-    stt_button = Button(label="Speak", width=100)
+    stt_button = Button(label="Говори! Parla!", width=100)
+
     stt_button.js_on_event("button_click", CustomJS(code="""
-     var recognition = new webkitSpeechRecognition();
-     recognition.continuous = true;
-     recognition.interimResults = true;
-      recognition.onresult = function (e) {
-         var value = "";
-         for (var i = e.resultIndex; i < e.results.length; ++i) {
-             if (e.results[i].isFinal) {
-                 value += e.results[i][0].transcript;
-             }
-         }
-          if ( value != "") {
-              document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-          }
-      }
-      recognition.start();
-      """))
-      result = streamlit_bokeh_events(
-          stt_button,
-          events="GET_TEXT",
-          key="listen",
-          refresh_on_update=False,
-          override_height=75,
-          debounce_time=0)
-      if result:
+        var recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
+     
+        recognition.onresult = function (e) {
+            var value = "";
+            for (var i = e.resultIndex; i < e.results.length; ++i) {
+                if (e.results[i].isFinal) {
+                    value += e.results[i][0].transcript;
+                }
+            }
+            if ( value != "") {
+                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+            }
+        }
+        recognition.start();
+        """))
+    
+    result = streamlit_bokeh_events(
+        stt_button,
+        events="GET_TEXT",
+        key="listen",
+        refresh_on_update=False,
+        override_height=75,
+        debounce_time=0)
+    
+    if result:
         if "GET_TEXT" in result:
-          st.write(result.get("GET_TEXT"))
-      #st.write(text)
+            st.write(result.get("GET_TEXT"))
+     #st.write(text)
