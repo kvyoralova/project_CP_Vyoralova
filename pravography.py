209dd56e184eea7f5c1c602cfa3d6eba6e9f86a2
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-05-29 19:25:38 +0200

    Update pravography.py

--- pravography.py
+++ pravography.py
@@ -1,25 +1,46 @@
 import streamlit as st
-import speech_recognition as sr
-import os
-import streamlit.components.v1 as components
 
 st.title('Pravografia: навчись добре писати італійською! Impara a scrivere bene in italiano!')
 st.subheader('Грайте, пишіть і вчіться! Gioca, scrivi e impara!')
 
 st.write("Скажи мені число від 1 до 10. Dimmi un numero da 1 a 10.")
 
-def audiorec_demo_app():
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 
-    parent_dir = os.path.dirname(os.path.abspath(__file__))
-    # Custom REACT-based component for recording client audio in browser
-    build_dir = os.path.join(parent_dir, "st_audiorec/frontend/build")
-    # specify directory and initialize st_audiorec object functionality
-    st_audiorec = components.declare_component("st_audiorec", path=build_dir)
+stt_button = Button(label="Speak", width=100)
 
-    # STREAMLIT AUDIO RECORDER Instance
-    st_audiorec()
-    
-if __name__ == '__main__':
-    # call main function
-    audiorec_demo_app()
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
 
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        number = result.get("GET_TEXT")
+        
+st.write(number)
